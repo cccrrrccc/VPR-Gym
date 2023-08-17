acfc9630a5b9f0a4a8c853f9126de389fa5644ad
crc <ruichen1@ualberta.ca>
2023-08-17 14:57:12 -0600

    Update the custom environment example

--- VprGym/blocktype_example.py
+++ VprGym/blocktype_example.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 	# Create a new environment
 	# VprEnv_blk_type is the custom VPR environment, whose action space is directed move type coupling with logical block type
 	# The action space is represented by a list [i, j]. (i = 0, ..., num_actions - 1 and j = 0, ..., num_types - 1) 
-	env = VprEnv_blk_type(inner_num = 0.1, port = '6000', seed = 1, arch = 'vtr_flow/arch/titan/stratixiv_arch.timing.xml', directory = 'example', benchmark = 'vtr_flow/benchmarks/titan_blif/stereo_vision_stratixiv_arch_timing.blif', reward_func = 'basic')
+	env = VprEnv_blk_type(inner_num = 0.1, port = '6666', seed = 1, arch = 'vtr_flow/arch/titan/stratixiv_arch.timing.xml', directory = 'example', benchmark = 'vtr_flow/benchmarks/titan_blif/stereo_vision_stratixiv_arch_timing.blif', reward_func = 'basic')
 	num_directed_moves = env.num_actions
 	num_block_types = env.num_types
 	avail_arms = []
@@ -15,6 +15,7 @@ if __name__ == '__main__':
 		for j in range(num_block_types):
 			avail_arms.append([i, j])
 	# done indicates whether the RL process is terminated or not
+	done = False
 	while (done == False):
 		action = avail_arms[randint(num_actions)] # Provide an action from agent, here random search is used as agent
 		_, reward, done, info = env.step(action) # pass the action to environment via env.step()
